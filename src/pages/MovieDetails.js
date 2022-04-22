4a1c48fcc9d27d066bce41993c3f61c050c0d2bc
milesl <lee.miles@sky.uk>
2022-04-22 15:07:52 +0100

    added movie poster to movie details screen

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -12,7 +12,7 @@ export class MovieDetails extends Lightning.Component {
       },
       Details: {
         x: 20,
-        y: 700,
+        y: 720,
         w: 1880,
         h: 360,
         rect: true,
@@ -26,26 +26,40 @@ export class MovieDetails extends Lightning.Component {
         //   {
         Title: {
           x: 20,
-          y: 30,
+          y: 80,
           text: {
             text: this.bindProp('title')
           }
         },
         ReleaseDate: {
           x: 20,
-          y: 80,
+          y: 140,
           text: {
             text: this.bindProp('releaseDate')
           }
         },
         Overview: {
           x: 20,
-          y: 130,
+          y: 200,
           w: 1800,
-          h: 400,
+          h: 380,
           text: {
             text: this.bindProp('overview')
           }
+        }
+      },
+      MovieImage: {
+        x: 20,
+        y: 100,
+        w: 500,
+        h: 620,
+        rect: true,
+        color: 0xaa111111,
+        Image: {
+          x: 20,
+          y: 20,
+          w: 450,
+          src: this.bindProp("movieImageURL")
         }
       }
     }
@@ -61,6 +75,7 @@ export class MovieDetails extends Lightning.Component {
   async useTheDetails(movieId) {
     const movieDetails = await getMovieDetails(movieId);
     console.log(`MovieDetails: movieDetails ${JSON.stringify(movieDetails)}`)
+    const moveImageURL = `https://image.tmdb.org/t/p/w440_and_h660_face${movieDetails.poster_path}`
     const backgroundPosterURL = `https://image.tmdb.org/t/p/w1066_and_h600_bestv2${movieDetails.backdrop_path}`
     this.tag("Background").patch({
       src: backgroundPosterURL
@@ -68,5 +83,6 @@ export class MovieDetails extends Lightning.Component {
     this.title = `${movieDetails.title}`;
     this.releaseDate = `Release Date: ${movieDetails.release_date}`;
     this.overview = movieDetails.overview;
+    this.movieImageURL = moveImageURL;
   }
 }
