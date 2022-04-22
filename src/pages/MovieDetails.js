2f778ac26145114e0725c6e3679c5494b42046e9
milesl <lee.miles@sky.uk>
2022-04-22 13:30:03 +0100

    added more movie details and put on a panel

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -10,11 +10,42 @@ export class MovieDetails extends Lightning.Component {
         h: 1080,
         color: 0xffffff00
       },
-      Title: {
-        x: 50,
-        y: 50,
-        text: {
-          text: ''
+      Details: {
+        x: 20,
+        y: 700,
+        w: 1880,
+        h: 360,
+        rect: true,
+        colorTop: 0xaa111111,
+        colorBottom: 0x00111111,
+        // flex: {
+        //   direction: 'row',
+        //   padding: 20,
+        // },
+        // children: [
+        //   {
+        Title: {
+          x: 20,
+          y: 20,
+          text: {
+            text: this.bindProp('title')
+          }
+        },
+        ReleaseDate: {
+          x: 20,
+          y: 70,
+          text: {
+            text: this.bindProp('releaseDate')
+          }
+        },
+        Overview: {
+          x: 20,
+          y: 130,
+          w: 1800,
+          h: 400,
+          text: {
+            text: this.bindProp('overview')
+          }
         }
       }
     }
@@ -32,12 +63,10 @@ export class MovieDetails extends Lightning.Component {
     console.log(`MovieDetails: movieDetails ${JSON.stringify(movieDetails)}`)
     const backgroundPosterURL = `https://image.tmdb.org/t/p/w1066_and_h600_bestv2${movieDetails.backdrop_path}`
     this.tag("Background").patch({
-      Title: {
-        text: {
-          text: movieDetails.title,
-        }
-      },
       src: backgroundPosterURL
-    })
+    });
+    this.title = `${movieDetails.title}`;
+    this.releaseDate = `Release Date: ${movieDetails.release_date}`;
+    this.overview = movieDetails.overview;
   }
 }
